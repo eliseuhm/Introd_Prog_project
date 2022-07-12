b784b7ffc01823035a8925a65e4e4abbf2c4f639
Eliseu <89275954+eliseuhm@users.noreply.github.com>
2022-07-12 14:54:11 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -39,9 +39,56 @@ int main(void)
         switch (agenda.menu)
         {
             case TELA_PRINCIPAL:
-                {
+            {
+                    printf("---------------------------------------\n");
+                    printf("\tAGENDA TELEFONICA:\n");
+                    printf("---------------------------------------\n");
+                    int opcao =-1;
+                    do
+                    {
+                        printf("Escolha uma opcao:\n");
+                        printf("\tdigite 1 para cadastrar contato\n");
+                        printf("\tdigite 2 para remover contato\n");
+                        printf("\tdigite 3 para listar todos os contatos\n");
+                        printf("\tdigite 0 para sair do programa\n");
+                        scanf("%d", &opcao);
+                        //int c = 0; //Solução empírica para esvaziamento de buffer
+                        //while ((c = getchar()) != '\n' && c != EOF) {}
+                    } while (opcao < 0 || opcao > 3);
 
-                }
+                    if (opcao == 0)
+                        agenda.menu = TELA_SAIR;
+
+                    if (opcao == 1)
+                    {
+                        agenda.menu = TELA_CADASTRAR;
+                        if (agenda.totalContatos >= MAX_CONTATOS)
+                        {
+                            printf("\tAgenda estah lotada, nao eh possivel adicionar novos contatos\n");
+                            agenda.menu = TELA_PRINCIPAL;
+                        }
+                    }
+
+                    if (opcao == 2)
+                    {
+                        agenda.menu = TELA_DESCADASTRAR;
+                        if (agenda.totalContatos == 0)
+                        {
+                            printf("\tNao hah nenhum contato cadastrado para remover\n");
+                            agenda.menu = TELA_PRINCIPAL;
+                        }
+                    }
+
+                    if (opcao == 3)
+                    {
+                        agenda.menu = TELA_LISTAR_TODOS;
+                        if (agenda.totalContatos == 0)
+                        {
+                            printf("\tAgenda nao tem nenhum contato para listar\n");
+                            agenda.menu = TELA_PRINCIPAL;
+                        }
+                    }
+            }
             break;
 
             case TELA_CADASTRAR:
